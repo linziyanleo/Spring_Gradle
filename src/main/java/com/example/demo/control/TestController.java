1329e94c90a1599c3fd98d46be5f9ca59a6bb89e
linziyanleo <linziyanleo@outlook.com>
2019-07-25 11:20:13 +0800

    add classes

---
+++ src/main/java/com/example/demo/control/TestController.java
@@ -0,0 +1,13 @@
+package com.example.demo.control;
+  
+import org.springframework.web.bind.annotation.RequestMapping;  
+import org.springframework.web.bind.annotation.RestController;  
+  
+@RestController  
+public class TestController {  
+  
+    @RequestMapping(value = "")  
+    public String test(){  
+        return "hello cueb";  
+    }  
+}  
