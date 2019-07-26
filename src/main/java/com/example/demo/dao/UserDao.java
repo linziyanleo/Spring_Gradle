aed17004ad5afcef66bddea1028a27b705bf5c14
linziyanleo <linziyanleo@outlook.com>
2019-07-26 10:57:01 +0800

    Add UserDao

---
+++ src/main/java/com/example/demo/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.demo.model.User;
+
+public interface UserDao extends CrudRepository<User, Long> {
+}
