f3f26a0e0814b14dddd49e967a6d19d1e609b5a6
linziyanleo <linziyanleo@outlook.com>
2019-07-26 11:25:43 +0800

    Finish JPA structure

--- src/main/java/com/example/demo/control/TestController.java
+++ src/main/java/com/example/demo/control/TestController.java
@@ -19,12 +19,6 @@ public class TestController {
     @Autowired
     private UserDao userDao;
 
-    /**
-     * 查询所有用户
-     * 
-     * @param model
-     * @return
-     */
     @GetMapping
     public ModelAndView list(Model model) {
         model.addAttribute("userList", userDao.findAll());
@@ -32,13 +26,6 @@ public class TestController {
         return new ModelAndView("user/list", "userModel", model);
     }
 
-    /**
-     * 根据id查询用户
-     * 
-     * @param id
-     * @param model
-     * @return
-     */
     @GetMapping("{id}")
     public ModelAndView view(@PathVariable("id") Long id, Model model) {
         User user = userDao.findOne(id);
@@ -47,12 +34,6 @@ public class TestController {
         return new ModelAndView("user/view", "userModel", model);
     }
 
-    /**
-     * 获取创建表单页面
-     * 
-     * @param model
-     * @return
-     */
     @GetMapping("/form")
     public ModelAndView createForm(Model model) {
         model.addAttribute("user", new User());
@@ -60,37 +41,18 @@ public class TestController {
         return new ModelAndView("user/form", "userModel", model);
     }
 
-    /**
-     * 保存或更新用户
-     * 
-     * @param user
-     * @return
-     */
     @PostMapping
     public ModelAndView saveOrUpdateUser(User user) {
         user = userDao.save(user);
         return new ModelAndView("redirect:/users");
     }
 
-    /**
-     * 删除用户
-     * 
-     * @param id
-     * @return
-     */
     @GetMapping("/delete/{id}")
     public ModelAndView delete(@PathVariable("id") Long id) {
         userDao.delete(id);
         return new ModelAndView("redirect:/users");// 重定向到list页面
     }
-
-    /**
-     * 获取修改用户的界面
-     * 
-     * @param id
-     * @param model
-     * @return
-     */
+    
     @GetMapping("/modify/{id}")
     public ModelAndView modify(@PathVariable("id") Long id, Model model) {
         User user = userDao.findOne(id);
