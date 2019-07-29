df25ae7e2236a5dbd027db36add25e68c9218ee4
linziyanleo <linziyanleo@outlook.com>
2019-07-29 10:27:17 +0800

    Update JPA DAO and Entity

---
+++ src/main/java/com/example/demo/dao/UserDao.java
@@ -0,0 +1,25 @@
+package com.example.demo.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.demo.entity.User;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface UserDao extends JpaRepository<User, Long> {
+
+	Page<User> findByNameContaining(String name, Pageable pageable);
+
+    List<User> findByNameContaining(String name);
+
+    List<User> findByNameContainingAndIdNot(String name, Long id);
+
+    List<User> findByIdNot(Long id);
+
+    Long countByAge(Integer age);
+
+}
